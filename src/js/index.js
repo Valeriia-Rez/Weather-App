a6254c795c4d68971840becb541dd6db0daf1407
Valeriia-Rez <Valeria09122016@gmail.com>
2020-04-02 19:22:34 +0300

    WIP

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ import "regenerator-runtime/runtime";
 import Weather from "./model/modelController";
 import viewWeather from "./view/viewController";
 
-const weather = new Weather("London");
+const weather = new Weather("Kharkiv");
 const view = new viewWeather();
 
 const renderWeather = async() => {
--- src/js/view/viewController.js
+++ src/js/view/viewController.js
@@ -4,9 +4,9 @@ export default class viewWeather {
         this.country = document.querySelector("[data-selector='country']");
         this.time = document.querySelector("[data-selector='time']");
         this.temperature_c = document.querySelector("[data-selector='temperature_c']");
-        this.temperature_f = document.querySelector("[data-selector='temperature_f']");
+
         this.feelslike_c = document.querySelector("[data-selector='feelslike_c']");
-        this.feelslike_f = document.querySelector("[data-selector='feelslike_f']");
+
         this.description = document.querySelector("[data-selector='description']");
         this.icon = document.querySelector("[data-selector='icon']");
         this.pressure = document.querySelector("[data-selector='pressure']");
@@ -15,17 +15,17 @@ export default class viewWeather {
 
     }
     displayResults(result) {
-        this.nameCity.textContent = result.location.name;
+        this.nameCity.textContent = `${result.location.name},`;
         this.country.textContent = result.location.country;
         this.time.textContent = result.location.localtime;
-        this.temperature_c.textContent = result.current.temp_c;
-        this.temperature_f.textContent = result.current.temp_f;
-        this.feelslike_c.textContent = result.current.feelslike_c;
-        this.feelslike_f.textContent = result.current.feelslike_f;
-        this.description.textContent = result.current.condition.text;
+        this.temperature_c.innerHTML = `<strong>${result.current.temp_c} &#176;C</strong>`;
+
+        this.feelslike_c.innerHTML = `<strong>Feels Like:</strong> ${result.current.feelslike_c} &#176;C`;
+
+        this.description.innerHTML = `<strong>${result.current.condition.text}</strong>`;
         this.icon.setAttribute("src", result.current.condition.icon);
-        this.pressure.textContent = `Pressure: ${result.current.pressure_mb} hPa`;
-        this.humidity.textContent = `Humidity: ${result.current.humidity}%`;
-        this.wind.textContent = `Wind: ${result.current.wind_mph} m/s`;
+        this.pressure.innerHTML = `<strong>Pressure:</strong> ${result.current.pressure_mb} hPa`;
+        this.humidity.innerHTML = `<strong>Humidity:</strong> ${result.current.humidity}%`;
+        this.wind.innerHTML = `<strong>Wind:</strong> ${result.current.wind_mph} m/s`;
     }
 }
