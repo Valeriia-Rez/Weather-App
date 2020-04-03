fd09863bcab34b865493ff34984a4a9351b93ace
Valeriia-Rez <Valeria09122016@gmail.com>
2020-04-03 10:43:13 +0300

    WIP

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,25 @@
 import "../scss/main.scss";
 import "regenerator-runtime/runtime";
-
 import Weather from "./model/modelController";
 import viewWeather from "./view/viewController";
 
 const weather = new Weather("Kharkiv");
 const view = new viewWeather();
 
-const renderWeather = async() => {
-    await weather.getWeather();
-    view.displayResults(weather.result);
 
+
+const changeWeather = () => {
+    const location = document.querySelector("[data-selector='location']").value;
+    weather.changeLocation(location);
+    renderWeather();
+    const location = "";
 
 }
 
+const renderWeather = async() => {
+    await weather.getWeather();
+    view.displayResults(weather.result);
+}
 
+document.querySelector("[data-selector='button']").addEventListener("click", changeWeather);
 window.addEventListener("DOMContentLoaded", renderWeather);
--- src/js/view/viewController.js
+++ src/js/view/viewController.js
@@ -4,24 +4,19 @@ export default class viewWeather {
         this.country = document.querySelector("[data-selector='country']");
         this.time = document.querySelector("[data-selector='time']");
         this.temperature_c = document.querySelector("[data-selector='temperature_c']");
-
         this.feelslike_c = document.querySelector("[data-selector='feelslike_c']");
-
         this.description = document.querySelector("[data-selector='description']");
         this.icon = document.querySelector("[data-selector='icon']");
         this.pressure = document.querySelector("[data-selector='pressure']");
         this.humidity = document.querySelector("[data-selector='humidity']");
         this.wind = document.querySelector("[data-selector='wind']");
-
     }
     displayResults(result) {
         this.nameCity.textContent = `${result.location.name},`;
         this.country.textContent = result.location.country;
         this.time.textContent = result.location.localtime;
         this.temperature_c.innerHTML = `<strong>${result.current.temp_c} &#176;C</strong>`;
-
         this.feelslike_c.innerHTML = `<strong>Feels Like:</strong> ${result.current.feelslike_c} &#176;C`;
-
         this.description.innerHTML = `<strong>${result.current.condition.text}</strong>`;
         this.icon.setAttribute("src", result.current.condition.icon);
         this.pressure.innerHTML = `<strong>Pressure:</strong> ${result.current.pressure_mb} hPa`;
